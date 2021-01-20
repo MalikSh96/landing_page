7f64e410d4263dc8dfdaebfeb91422ea5acca02b
MalikSh96 <malik96sharfo@hotmail.com>
2021-01-20 15:37:51 +0100

    part 2 completed, animated hamburger menu, issues with mobile version not showing logo and hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,21 @@
-console.log("Hello world!");
+//console.log("Hello world!");
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function () {
+    console.log('click hamburger');
+    if (header.classList.contains('open')) { //Closing the hamburger menu
+        //remove it
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+
+    }
+    else { //opening the hamburger menu
+        //add other class
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+});
